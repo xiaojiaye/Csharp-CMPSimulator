eb8568bac17afce6e0c9801f3325e4a017477396
jiakin <jk39y93@126.com>
2020-07-02 16:58:45 +0800

    【20200709】16:58 at office R513V

---
+++ CMPSimulator/Entity/QueryReturnEntity/QRYGJDTL/QrygjdtlOut.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace CMPSimulator.Entity.QueryReturnEntity.QRYGJDTL
+{
+    [XmlRootAttribute("out")]
+    public class QrygjdtlOut
+    {
+        [XmlElementAttribute("AcctName")]
+        public string AcctName { get; set; }
+        [XmlElementAttribute("AcctNo")]
+        public string AcctNo { get; set; }
+        [XmlElementAttribute("CurrType")]
+        public string CurrType { get; set; }
+        [XmlElementAttribute("StartDate")]
+        public string StartDate { get; set; }
+        [XmlElementAttribute("EndDate")]
+        public string EndDate { get; set; }
+        [XmlElementAttribute("NextTag")]
+        public string NextTag { get; set; }
+        [XmlElementAttribute("AcctSeq")]
+        public string AcctSeq { get; set; }
+
+
+    }
+}
---
+++ CMPSimulator/Entity/QueryReturnEntity/QRYGJDTL/rd.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace CMPSimulator.Entity.QueryReturnEntity.QRYGJDTL
+{
+    // srcXml 中的字段个数可以比rd中的属性多，没有的就不会解析。
+    // 属性类型都要是string，都要有[XmlElementAttribute("Drcrf")]说明
+    // 其中有一个int或者没有xml注解，就好string 转 classObject 失败
+    [XmlRootAttribute("rd", IsNullable = false)]
+    public class rd
+    {
+     
+    }
+}
